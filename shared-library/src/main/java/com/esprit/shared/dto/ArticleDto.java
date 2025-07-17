ffb5fd134fdd74afedb17457f63eb7cca60e4509
Aymen-Ben-Rached <aymen.benrached@esprit.tn>
2025-07-17 11:04:51 +0100

    Implement WebClient, POST /articles now checks if stock is > 0, decrements stock if true

--- shared-library/src/main/java/com/esprit/shared/dto/ArticleDto.java
+++ shared-library/src/main/java/com/esprit/shared/dto/ArticleDto.java
@@ -1,3 +1,3 @@
 package com.esprit.shared.dto;
 
-public record ArticleDto(Long id, String title, String content, int stockQuantity) {}
+public record ArticleDto(Long id, String title, String content) {}
